1088db802efa2802eca68575f75d6480990b6ae0
coder-arjab <arjabkhadka93@gmail.com>
2020-02-02 19:00:07 +0545

    added server site computing

--- hackathan.py
+++ hackathan.py
@@ -1,5 +1,9 @@
 from tkinter import *
+import pyttsx3
+import wikipedia
 
+
+s=pyttsx3.init()
 
 global guides
 guides=[]
@@ -11,40 +15,10 @@ global places
 places=[]
 
 #name,district,latitude,longitude,rate_history
-places=[["Kathmandu","Kathmandu",[27.7172, 85.3240],[3,4,3,4,3,4,3,4]],
-    ["Pokhara Valley","Pokahara",[28.2096, 83.9856],[2,4,5,4,4,5]],
-    ["Bhaktapur","Bhaktapur",[27.6710, 85.4298],[4,3,2,1]],
-    ["Pashupatinath","Kathmandu",[27.7105, 85.3487],[5,1,4,3]],
-    ["Boudhanath","Kathmandu",[27.7214, 85.3620],[5,4,2,1]],
-    ["Nagarkot","Kathamndu",[27.7174, 85.5046],[3,4,5,1]],
-    ["Lumbini","Rupandehi",[27.6792, 83.5070],[5,4,5,4,5,5]]
-    [""]]
 
 class guide:
+
     def __init__(self,name,address,age,email,passkey,position,skill,desp_ur,liscence_no,contact,sex,lang,busy,rating):
-        self.name=name
-        self.address=address
-        self.email=email
-        self.position=position
-        self.skill=skill
-        self.age=age
-        self.desp_ur=desp_ur
-        self.liscence_no=liscense_no
-        self.contact=contact
-        self.sex=sex
-        self.lang=lang
-        self.busy=busy
-        self.rating=rating
-        Add(self.name,self.address,self.email,self.position,self.skill,self.age,self.deep_ur,self.liscence,self.contact,self.sex,self.lang,self.busy,rating)        
-
-    def Add(name,address,email,position,skill,desp_ur,liscence_no,contact,sex,lang,busy):
-        if (len(name)!=0 and len(address)!=0 and age>17 and len(liscence_no)==12 and len(position)!=0 and len(contact)!=0 and rating<=5):
-            #formally adding to the list
-            guides.append([name,address,email,passkey,position,skill,desp_ur,liscence_no,contact,sex,lang,busy,rating])
-            return ("A with "+str(name)+" was added")
-
-class user:
-    def __init__(self,name,address,age,email,passkey,position,skill,desp_ur,liscence_no,contact,sex,lang,busy):
         self.name=name
         self.address=address
         self.email=email
@@ -53,21 +27,110 @@ class user:
         self.skill=skill
         self.age=age
         self.desp_ur=desp_ur
+        self.liscence=liscence_no
         self.contact=contact
         self.sex=sex
         self.lang=lang
         self.busy=busy
-        Add(self.name,self.address,self.email,self.passkey,self.position,self.skill,self.age,self.deep_ur,self.liscence,self.contact,self.sex,self.lang,self.busy)
+        self.rating=rating
+        guide.Add(self.name,self.address,self.email,self.position,self.skill,self.age,self.desp_ur,self.liscence,self.contact,self.sex,self.lang,self.busy,self.rating)        
 
+    def Add(name,address,email,position,skill,age,desp_ur,liscence_no,contact,sex,lang,busy,rating):
+        if (len(name)!=0 and len(address)!=0 and int(age)>17 and len(liscence_no)>8 and len(position)!=0 and len(contact)!=0):
+            print('true')
+            guides.append([name,address,email,position,skill,age,desp_ur,liscence_no,contact,sex,lang,busy,rating])
+            return ("A with "+str(name)+" was added")
+
+class user:
+    def __init__(self,name,address,age,email,passkey,desp_ur,contact,sex,lang):
+        self.name=name
+        self.address=address
+        self.email=email
+        self.passkey=passkey
+        self.age=age
+        self.desp_ur=desp_ur
+        self.contact=contact
+        self.sex=sex
+        self.lang=lang
+        user.Add(self.name,self.address,self.email,self.passkey,self.age,self.desp_ur,self.contact,self.sex,self.lang)
+
+    def Add(name,address,email,passkey,age,desp_ur,contact,sex,lang):
+        if (len(name)!=0 and len(address)!=0 and int(age)>17 and len(contact)!=0):
+            #formally adding to the list
+            users.append([name,address,email,passkey,age,desp_ur,contact,sex,lang])
+            return ("A with "+str(name)+" was added")
 
 
 class place:
-    def __init__(self,name,district,latitude,longitude,rate):
+    def __init__(self,name,district,latitude,longitude,rate,tag):
         self.name=name
-        self.district=disctrict
+        self.district=district
         self.longitude=longitude
         self.latitude=latitude
         self.history=rate
         self.rate=sum(rate)/len(rate)
+        self.tag=tag
+        place.add(self.name,self.district,self.longitude,self.longitude,self.history,self.rate,self.tag)
 
-    def getdistance(platlong,ulatlong)
+    def add(name,district,latitude,longitude,history,rate,tag):
+        if (latitude!=0 and longitude!=0 and name!="0"):
+            places.append([name,district,latitude,longitude,history,rate,tag])
+
+    def getwikipediainfo(query):
+        sum1=wikipedia.summary(query,sentences=1)
+        sum2=wikipedia.summary(query)
+        print(sum2)
+        speak(sum1)
+
+    def getdistance(platlong,ulatlong):
+        ux=float(ulatlong[0])
+        uy=float(ulatlong[1])
+        px=float(platlong[0])
+        py=float(plytlong[1])
+        distance=((ux-px)^2+(uy-py)^2)^(1/2)
+        return distance
+
+
+def speak(x):
+    s.say(x)
+    s.runAndWait()
+
+#place.getwikipediainfo("Lumbini")
+
+#name,address,age,email,passkey,desp_ur,liscence_no,contact,sex,lang,busy
+user("Harry","London","19","harrydon@gmail.com","harrylovespreeti","i am harry from wonderland","harry's contact","m",["japanese","bhutanese","english"])
+
+user("Carry","Houston","29","carrym@gmail.com","helloworld","i am a good boy","carry's contact","m",["french","english"])
+
+user("Lauren","Kameski","19","lauren1@gmail.com","mercyplease","kindful to all","lauren's contact","f",["korean","chinese","english"])
+
+user("Samira","Kaski","34","samirpok@gmail.com","samirkills","i love everyone","samira's contact","f",["japanese","chinese","nepalese"])
+
+#self,name,address,age,email,passkey,position,skill,desp_ur,liscence_no,contact,sex,lang,busy,rating)
+
+guide("Pratap","patan",19,"pratappokheral@gmail.com",'pratap_rocks','driver','10 +2','pratap is a lonely boy','890890890890','9841123456','m',['hindi','bhojpuri','french'],False,'4')
+
+guide("Hiptap","pokhara",19,"hittappokheral@gmail.com",'hittaprocks','retired officer','masters','mero desh mero nepal','123123123123','9841823456','m',['hindi','english','french'],False,'4.3')
+
+#self,name,district,latitude,longitude,rate,tag
+place("Butwal","Rupandehi",27.6866, 83.4323,[3,4,3,2,1],['park','lumbini'])
+place("Nagarkot","Kathamndu",27.7174, 85.5046,[3,4,5,1],['cycling','photography','adventure','sight seeing'])
+place("Lumbini","Rupandehi",27.6792, 83.5070,[5,4,5,4,5,5],['cycling','photography','buddhism','sight seeing'])
+place("Boudhanath","Kathmandu",27.7214, 85.3620,[5,4,2,1],['religious','spirituality','photography'])
+
+print("USER===============================")
+for us in users:
+    print (us)
+    print ("\n \n")
+    
+print("guides===============================")
+
+for guid in guides:
+    print (guid)
+    print ("\n \n")
+
+print("PLACES===============================")
+
+for plac in places:
+    print(plac)
+    print("\n \n")
