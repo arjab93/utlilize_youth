9666f6e1114f60d8d88362d587387e784cdbe510
coder-arjab <arjabkhadka93@gmail.com>
2020-02-03 12:38:56 +0545

    Final Drama

--- flask_hack.py
+++ flask_hack.py
@@ -1,9 +1,9 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,redirect
 app=Flask(__name__)
 
 #posts=['nirjal','bikram','jell']
 
-from tkinter import *
+
 import pyttsx3
 import wikipedia
 import threading
@@ -19,8 +19,10 @@ users=[]
 global places
 places=[]
 
-#name,district,latitude,longitude,rate_history
+global msgpool
+msgpool=[]
 
+#name,district,latitude,longitude,rate_history
 class guide:
     def __init__(self,name,address,age,email,passkey,position,skill,desp_ur,liscence_no,contact,sex,lang,busy,rating):
         self.name=name
@@ -62,13 +64,14 @@ class user:
         if (len(name)!=0 and len(address)!=0 and int(age)>17 and len(contact)!=0):
             #formally adding to the list
             users.append([name,address,email,passkey,age,desp_ur,contact,sex,lang])
-            print ("A with "+str(name)+" was added")
+            print ("A block was added with \n")
+            print (users[length(users)-1]) 
         else:
             raise Exception
 
 
 class place:
-    def __init__(self,name,district,latitude,longitude,rate,tag):
+    def __init__(self,name,district,latitude,longitude,rate,tag,username):
         self.name=name
         self.district=district
         self.longitude=longitude
@@ -76,11 +79,20 @@ class place:
         self.history=rate
         self.rate=round(sum(rate)/len(rate),2)
         self.tag=tag
-        place.Add(self.name,self.district,self.longitude,self.longitude,self.history,self.rate,self.tag)
+        self.username=username
+        place.Add(self.name,self.district,self.longitude,self.longitude,self.history,self.rate,self.tag,self.username)
 
-    def Add(name,district,latitude,longitude,history,rate,tag):
-        if (latitude!=0 and longitude!=0 and name!="0"):
-            places.append([name,district,latitude,longitude,history,rate,tag])
+    def isValid(username):
+        for user in users:
+            
+            if user[0]==username:
+                return True
+        
+        return False
+
+    def Add(name,district,latitude,longitude,history,rate,tag,username):
+        if (latitude!=0 and longitude!=0 and name!="0" and place.isValid(username)):
+            places.append([name,district,latitude,longitude,history,rate,tag,username])
             return True
         else:
             raise Exception
@@ -107,14 +119,20 @@ def speak(x):
 
 #place.getwikipediainfo("Lumbini")
 
+
+
 #name,address,age,email,passkey,desp_ur,liscence_no,contact,sex,lang,busy
 user("Harry","London","19","harrydon@gmail.com","harrylovespreeti","i am harry from wonderland","harry's contact","m",["japanese","bhutanese","english"])
+
+user("Nirjal","London","19","nirjal@gmail.com","nirjalrocks","i am harry from wonderland","harry's contact","m",["japanese","bhutanese","english"])
 
 user("Carry","Houston","29","carrym@gmail.com","helloworld","i am a good boy","carry's contact","m",["french","english"])
 
 user("Lauren","Kameski","19","lauren1@gmail.com","mercyplease","kindful to all","lauren's contact","f",["korean","chinese","english"])
 
 user("Samira","Kaski","34","samirpok@gmail.com","samirkills","i love everyone","samira's contact","f",["japanese","chinese","nepalese"])
+
+
 
 #self,name,address,age,email,passkey,position,skill,desp_ur,liscence_no,contact,sex,lang,busy,rating)
 
@@ -123,10 +141,10 @@ guide("Hiptap","pokhara",19,"hittappokheral@gmail.com",'hittaprocks','retired of
 guide("hittap","pokhara",19,"hittappaudel@gmail.com",'hittaprockz','coffee maker','masters','mero desh mero country','123123123123','9848823456','m',['hindi','jeum','french'],False,'4.3')
 
 #self,name,district,latitude,longitude,rate,tag
-place("Butwal","Rupandehi",27.6866, 83.4323,[3,4,3,2,1],['park','lumbini'])
-place("Nagarkot","Kathamndu",27.7174, 85.5046,[3,4,5,1],['cycling','photography','adventure','sight seeing'])
-place("Lumbini","Rupandehi",27.6792, 83.5070,[5,4,5,4,5,5],['cycling','photography','buddhism','sight seeing'])
-place("Boudhanath","Kathmandu",27.7214, 85.3620,[5,4,2,1],['religious','spirituality','photography'])
+place("Butwal","Rupandehi",27.6866, 83.4323,[3,4,3,2,1],['park','lumbini'],"Nirjal")
+place("Nagarkot","Kathamndu",27.7174, 85.5046,[3,4,5,1],['cycling','photography','adventure','sight seeing'],"Nirjal")
+place("Lumbini","Rupandehi",27.6792, 83.5070,[5,4,5,4,5,5],['cycling','photography','buddhism','sight seeing'],"Nirjal")
+place("Boudhanath","Kathmandu",27.7214, 85.3620,[5,4,2,1],['religious','spirituality','photography'],"Nirjal")
 links=['butwal','nagarkot','lumbini','boudhanath']
 
 
@@ -142,22 +160,11 @@ links=['butwal','nagarkot','lumbini','boudhanath']
     
 
 '''
-@app.route('/<place>')
-def displayplace(place):
-    
-    info=place.getwikipediainfo(place)
-    threading.Thread(name="wikipedia",target=speak,args=info[0]).start()
-    body_content=info[1]
-    title=info[2]
-    return render_template('layout_ans.html',
-        body_content=body_content,
-        title=title)
-
 
 #displayplace('Barhabise')
 
 @app.route('/signup_guide/', methods=['post', 'get'])
-def login_guide():
+def signup_guide():
     message = 'GUIDES MUST PROVIDE VALID INFORMATION'
     if request.method == 'POST':
         name = request.form.get('username')
@@ -196,21 +203,25 @@ def addplace():
         district = request.form.get('address')
         latitude =request.form.get('latitude') 
         longitude = request.form.get('longitude')
-        rates=[2.5,2.5,2.5]  
+        username=request.form.get('userkoname')
+        rates=[0,0,0,0]  
         rate=round(sum(rates)/len(rates),2)
         tags=request.form.get('tags')
         tags=tags.split(',')
-        print((name,district,latitude,longitude,rate,tags))
+        print((name,district,latitude,longitude,rate,tags,username))
+        
         try:
-            print((name,district,latitude,longitude,rates,rate,tags))
-            place.Add(name,district,latitude,longitude,rates,rate,tags)
-            
+            print((name,district,latitude,longitude,rates,rate,tags,username))
+            place.Add(name,district,latitude,longitude,rates,rate,tags,username)            
+        
         except Exception as e :
             print (e)
             print("\n")
             print("ERROr OCCURED")
             message=message+"ERRROR IN THE ENTERED DATA"  
+    
     return render_template('enterplace.html', message=message)
+
 
 @app.route('/signup_user/', methods=['post', 'get'])
 def signup_user():
@@ -236,8 +247,6 @@ def signup_user():
     return render_template('user_signup.html', message=message)
 
 
-
-
 @app.route("/butwal")
 def butwal():
     wiki=place.getwikipediainfo("butwal")
@@ -258,7 +267,65 @@ def nagarkot():
     head=wiki[2]
     threading.Thread(name="get",target=speak,args=(x,)).start()
     return render_template("desp.html",body=body,head=head)
+
+global useris
+useris=[]
+
+#guide("Pratap","patan",19,"pratappokheral@gmail.com",'pratap_rocks','driver','10 +2','pratap is a lonely boy','890890890890','9841123456','m',['hindi','bhojpuri','french'],False,'4')
+@app.route('/login_user/', methods=['post', 'get'])
+def login():
+    if request.method == 'POST':
+        emu= request.form.get('emu')
+        passw=request.form.get('passkey')
+        #matching emu
+        i=0
+        for user in users:
+            username=user[0]
+            email=user[3]
+            password=user[5]
+
+            if ((emu==username or email==emu) and password==passw):
+                useris=user
+                print(useris)
+                return redirect(url_for('msgpool'),useris=useris)
+        
+        for user in guides:
+            username=user[0]
+            email=user[3]
+            password=user[5]
+
+            if ((emu==username or email==emu) and password==passw):
+                useris=user
+                return redirect(url_for('msgpool'),useris=useris)
+        
+
+
+        
+    return render_template('login.html')
+
+
+
+
+@app.route('/msgpool',methods=['post','get'])
+def pool():
+    i=0
+    if request.method == 'POST':
+        if (request.form.get('Yes') == 'Yes'):
+            msgpool.remove(msgpool[i])
+
     
+    for place in msgpool:
+        i=i+1
+        return render_template("msgpool.html",place=place)   
+
+
+
+for place in places:
+    if (place[5]==0):
+        msgpool.append([place])
+
+
+# if request.form.get('Encrypt') == 'Encrypt':
 
 @app.route("/lumbini")
 def lumbini():
@@ -281,16 +348,16 @@ def boudhanath():
     threading.Thread(name="get",target=speak,args=(x,)).start()
     return render_template("desp.html",body=body,head=head)
 
+
 @app.route("/")
 @app.route("/home")
 def home():
     return render_template('home.html')
 
+
 @app.route("/about")
 def abt():
-    return render_template('about.html',title="HELL YEAH")
-
-
+    return render_template('home.html')
 
 if __name__=="__main__":
     app.run(debug=True)
